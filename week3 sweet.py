e0d7114ac35594937febd9af60a421873e3ecdce
tmfrl12 <67643669+tmfrl12@users.noreply.github.com>
2020-08-02 16:10:33 +0900

    Update week3 sweet.py
    업데이트 버전
    배운점은 옛날 버전에 있다.

--- week3 sweet.py
+++ week3 sweet.py
@@ -1,18 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
-from pymongo import MongoClient  # pymongo를 임포트 하기(패키지 인스톨 먼저 해야겠죠?)
+from pymongo import MongoClient
 
-client = MongoClient('localhost', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
-db = client.dbsparta    # 'dbsparta'라는 이름의 db를 사용합니다. 'dbsparta' db가 없다면 새로 만듭니다.
+client = MongoClient('localhost', 27017)
+db = client.dbsparta
 
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-# 아래 빈 칸('')을 채워보세요
+
 data = requests.get('https://www.genie.co.kr/chart/top200?ditc=D&rtm=N&ymd=20200713', headers=headers)
 soup = BeautifulSoup(data.text, 'html.parser')
 
 musics = soup.select('#body-content > div.newest-list > div > table > tbody > tr')
+
 for music in musics:
     rank = music.select_one('td.number').text[0:2].strip()
     title = music.select_one('td.info > a.title.ellipsis').text.strip()
